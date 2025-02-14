b966d3dfcfaae834d7db6d5a0be072e4b425a088
Sean Fioritto <sean@planningforaliens.com>
2025-02-14 12:49:46 -0600

    Add files method placeholder

--- packages/extension-files/index.ts
+++ packages/extension-files/index.ts
@@ -1,6 +1,6 @@
 import { type Workflow, createExtension } from "@positronic/core";
 import type { State } from "@positronic/core";
-const filesExtension = createExtension('files', {
+const filesExtension = createExtension('fs', {
   file(
     this: Workflow<any>,
     title: string,
@@ -17,12 +17,20 @@ const filesExtension = createExtension('files', {
         }
       };
     });
+  },
+  files(
+    this: Workflow<any>,
+    title: string,
+    files: {
+      [key: string]: string;
+  }) {
+    return this.step('files', ({ state }) => ({ ...state, files }));
   }
 });
 
 declare module "@positronic/core" {
   interface Workflow<TOptions extends object, TState extends State> {
-    files: ReturnType<typeof filesExtension.augment<TOptions, TState>>;
+    fs: ReturnType<typeof filesExtension.augment<TOptions, TState>>;
   }
 }
 
