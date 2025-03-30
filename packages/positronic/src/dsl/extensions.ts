c751e588d8d0618eac7c9bfa67ab806a4d38238b
Sean Fioritto <sean@planningforaliens.com>
2025-03-30 11:38:29 -0500

    Remove unused types

--- packages/positronic/src/dsl/extensions.ts
+++ packages/positronic/src/dsl/extensions.ts
@@ -1,39 +1,4 @@
 import { Workflow } from "./workflow";
-import type { State } from "./types";
-
-type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
-
-type ExtensionMethods<
-  TExtension extends Record<string, any>,
-  TOptions extends object,
-  TState extends State
-> = TExtension extends ((...args: any[]) => any)
-  ? TExtension extends ((
-      this: any,
-      title: string,
-      config: infer TConfig
-    ) => Workflow<any, infer TReturnState>)
-    ? (
-        title: string,
-        config: TConfig extends ((ctx: any) => any)
-          ? { [P in keyof TConfig]: TConfig[P] extends Function ? ((ctx: TState) => any) : TConfig[P] }
-          : TConfig
-      ) => Workflow<TOptions, Expand<TState & TReturnState>>
-    : never
-  : {
-      [K in keyof TExtension]: TExtension[K] extends (
-        this: any,
-        title: string,
-        config: infer TConfig
-      ) => Workflow<any, infer TReturnState>
-        ? (
-            title: string,
-            config: TConfig extends ((ctx: any) => any)
-              ? { [P in keyof TConfig]: TConfig[P] extends Function ? ((ctx: TState) => any) : TConfig[P] }
-              : TConfig
-          ) => Workflow<TOptions, Expand<TState & TReturnState>>
-        : never;
-    };
 
 export function createExtension<
   TExtensionKey extends string,
