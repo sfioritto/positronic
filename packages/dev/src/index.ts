f907a0ca3e97f8c5405ac6a6a68807a931843efa
Sean Fioritto <sean@planningforaliens.com>
2025-03-31 10:55:36 -0500

    Dont'rename the export

--- packages/dev/src/index.ts
+++ packages/dev/src/index.ts
@@ -1,7 +1,3 @@
 import { LocalResourceLoader } from './local-resource-loader';
 
-// Export the local implementation as the default ResourceLoader
-export const ResourceLoader = LocalResourceLoader;
-
-// Also export the named implementation for those who need the specific class
 export { LocalResourceLoader };
