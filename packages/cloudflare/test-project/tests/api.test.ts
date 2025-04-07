42c18b817ad4ecee947759fa724e81a5be217edf
Sean Fioritto <sean@planningforaliens.com>
2025-04-06 20:15:46 -0500

    Start with a simpler vitest example

---
+++ packages/cloudflare/test-project/tests/api.test.ts
@@ -0,0 +1,24 @@
+import {
+  env,
+  createExecutionContext,
+  waitOnExecutionContext,
+} from "cloudflare:test";
+import { describe, it, expect } from "vitest";
+// Could import any other source file/function here
+import worker from "../src";
+
+// For now, you'll need to do something like this to get a correctly-typed
+// `Request` to pass to `worker.fetch()`.
+const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+
+describe("Hello World worker", () => {
+  it("responds with Hello World!", async () => {
+    const request = new IncomingRequest("http://example.com");
+    // Create an empty context to pass to `worker.fetch()`
+    const ctx = createExecutionContext();
+    const response = await worker.fetch(request, env, ctx);
+    // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+    await waitOnExecutionContext(ctx);
+    expect(await response.text()).toBe("Hello World!");
+  });
+});
