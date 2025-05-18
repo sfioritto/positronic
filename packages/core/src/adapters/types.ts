0dc3ddc6b98879cc9a922d13e38c06e1e5c4eadb
Sean Fioritto <sean@planningforaliens.com>
2025-05-18 13:37:23 -0500

    Clean up the adapter type

--- packages/core/src/adapters/types.ts
+++ packages/core/src/adapters/types.ts
@@ -1,12 +1,5 @@
-import { WORKFLOW_EVENTS } from '../dsl/constants.js';
 import type { WorkflowEvent } from '../dsl/workflow.js';
 
 export interface Adapter<Options extends object = any> {
-  started?(event: WorkflowEvent<Options>): void | Promise<void>;
-  updated?(event: WorkflowEvent<Options>): void | Promise<void>;
-  completed?(event: WorkflowEvent<Options>): void | Promise<void>;
-  error?(event: WorkflowEvent<Options>): void | Promise<void>;
-  restarted?(event: WorkflowEvent<Options>): void | Promise<void>;
-
   dispatch(event: WorkflowEvent<Options>): void | Promise<void>;
 }
