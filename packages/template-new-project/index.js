0541810577a19f9fd9616f215faab54ef02c0418
Sean Fioritto <sean@planningforaliens.com>
2025-07-20 14:12:49 -0500

    Update to .0.0.3

--- packages/template-new-project/index.js
+++ packages/template-new-project/index.js
@@ -53,9 +53,9 @@ module.exports = {
   ],
   setup: async ctx => {
     const devRootPath = process.env.POSITRONIC_LOCAL_PATH;
-    let coreVersion = 'latest';
-    let cloudflareVersion = 'latest';
-    let clientVercelVersion = 'latest';
+    let coreVersion = '^0.0.3';
+    let cloudflareVersion = '^0.0.3';
+    let clientVercelVersion = '^0.0.3';
 
     // Map backend selection to package names
     const backendPackageMap = {
