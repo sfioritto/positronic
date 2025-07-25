c561d110a919e3d1cf0bab7eab605c764414ca59
Sean Fioritto <sean@planningforaliens.com>
2025-07-25 12:24:41 -0500

    Bump to v0.0.6

--- packages/template-new-project/index.js
+++ packages/template-new-project/index.js
@@ -53,9 +53,9 @@ module.exports = {
   ],
   setup: async ctx => {
     const devRootPath = process.env.POSITRONIC_LOCAL_PATH;
-    let coreVersion = '^0.0.5';
-    let cloudflareVersion = '^0.0.5';
-    let clientVercelVersion = '^0.0.5';
+    let coreVersion = '^0.0.6';
+    let cloudflareVersion = '^0.0.6';
+    let clientVercelVersion = '^0.0.6';
 
     // Map backend selection to package names
     const backendPackageMap = {
